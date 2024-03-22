ae8d2ec9a8c550a97145f1377b39ee65fd6bec66
LEANDRO VINICIUS FEIJO MELO <122054743+leeehvinicius@users.noreply.github.com>
2024-03-22 11:36:53 -0400

    criaçao da tela generica de login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,5 +10,15 @@ import { DefaultLoginLayoutComponent } from '../../components/default-login-layo
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginform!: FormGroup;
+
+
+  constructor(){
+    this.loginform = new FormGroup({
+      email: new FormControl('', [Validators.email,Validators.required]),
+      password: new FormControl('', [Validators.required,Validators.minLength(6)])
+    })
+
+  }
 
 }
